a1c0e4036fa7b0ff776c1d2e94c4c78ea774d622
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-06-03 02:09:24 +0600

    hero section, video popup and edit navbar

---
+++ home/static/home/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    // !Navbar Shrink
+    $(window).on("scroll",function(){
+        if($(this).scrollTop() > 90){
+            $(".navbar").addClass("navbar-shrink");
+        }
+        else{
+            $(".navbar").removeClass("navbar-shrink");
+        }
+    });
+
+    // !Video Popup
+    const videoSrc = $('#player-1').attr("src");
+    $(".video-play-btn, .video-popup").on("click", function(){
+        if($(".video-popup").hasClass("open")){
+            $(".video-popup").removeClass("open");
+            $("#player-1").attr("src","");
+        }
+        else{
+            $(".video-popup").addClass("open");
+            if($("#player-1").attr("src")==''){
+                $("#player-1").attr("src", videoSrc);
+            }
+        }
+    });
+});
