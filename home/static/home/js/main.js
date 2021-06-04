8892faafd8c710945e76b714144758d2cfdc5c7c
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-06-04 22:36:15 +0600

    clients section

--- home/static/home/js/main.js
+++ home/static/home/js/main.js
@@ -1,64 +1,81 @@
 $(document).ready(function () {
-    // !Navbar Shrink
-    $(window).on("scroll", function () {
-        if ($(this).scrollTop() > 90) {
-            $(".navbar").addClass("navbar-shrink");
-        }
-        else {
-            $(".navbar").removeClass("navbar-shrink");
-        }
-    });
+  // !Navbar Shrink
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 90) {
+      $(".navbar").addClass("navbar-shrink");
+    } else {
+      $(".navbar").removeClass("navbar-shrink");
+    }
+  });
 
-    // !Video Popup
-    const videoSrc = $('#player-1').attr("src");
-    $(".video-play-btn, .video-popup").on("click", function () {
-        if ($(".video-popup").hasClass("open")) {
-            $(".video-popup").removeClass("open");
-            $("#player-1").attr("src", "");
-        }
-        else {
-            $(".video-popup").addClass("open");
-            if ($("#player-1").attr("src") == '') {
-                $("#player-1").attr("src", videoSrc);
-            }
-        }
-    });
+  // !Video Popup
+  const videoSrc = $("#player-1").attr("src");
+  $(".video-play-btn, .video-popup").on("click", function () {
+    if ($(".video-popup").hasClass("open")) {
+      $(".video-popup").removeClass("open");
+      $("#player-1").attr("src", "");
+    } else {
+      $(".video-popup").addClass("open");
+      if ($("#player-1").attr("src") == "") {
+        $("#player-1").attr("src", videoSrc);
+      }
+    }
+  });
 
-    // !Features Carousel
-    $('.features-carousel').owlCarousel({
-        loop: true,
-        margin: 0,
-        autoplay: true,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 2,
-            },
-            1000: {
-                items: 3,
-            }
-        }
-    });
+  // !Features Carousel
+  $(".features-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
 
-    // !Screenshots Carousel
-    $('.screenshots-carousel').owlCarousel({
-        loop: true,
-        margin: 0,
-        autoplay: true,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 2,
-            },
-            1000: {
-                items: 4,
-            }
-        }
-    });
+  // !Screenshots Carousel
+  $(".screenshots-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
+
+  // !Testimonials Carousel
+  $(".testimonials-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
 });
