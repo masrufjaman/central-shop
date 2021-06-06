1ec5457b219e5b81f4a94c2f90f79efe94855163
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-06-06 15:11:30 +0600

    team section

--- home/static/home/js/main.js
+++ home/static/home/js/main.js
@@ -78,4 +78,23 @@ $(document).ready(function () {
       },
     },
   });
+
+  // !Team Carousel
+  $(".team-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
 });
