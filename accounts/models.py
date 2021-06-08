3ac047161451bbedda31bb034d3e66d3b7c62b12
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-06-09 00:25:24 +0600

    edit model

--- accounts/models.py
+++ accounts/models.py
@@ -16,19 +16,42 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     digital = models.BooleanField(default=False, null=True, blank=False)
-    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    # image
 
     def __str__(self):
         return self.name
 
 
 class Order(models.Model):
-    STATUS = (
-        ('Pending', 'Pending'),
-        ('Out of delivery', 'Out of delivery'),
-        ('Delivered', 'Delivered'),
-    )
-    # customer
-    # product
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
-    status = models.CharField(max_length=200, null=True, choices=STATUS)
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_orderd = models.DateTimeField(auto_now_add=True, null=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(
+        Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(
+        Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
