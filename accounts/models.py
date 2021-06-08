7bf97f3339e89724cf0e081983f46dc9add35530
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-06-08 23:56:30 +0600

    edit customer and product model

--- accounts/models.py
+++ accounts/models.py
@@ -1,26 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Customer(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
-    phone = models.CharField(max_length=200, null=True)
-    email = models.EmailField(max_length=200, null=True)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    email = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    CATEGORY = (
-        ('Indoor', 'Indoor'),
-        ('Out Door', 'Out Door'),
-    )
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
+    digital = models.BooleanField(default=False, null=True, blank=False)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    discription = models.CharField(max_length=200, null=True)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
