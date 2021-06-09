e71cfd806111d22565a4097ccb7354c8376c6f97
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-06-09 18:23:11 +0600

    views for cart products

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from .models import *
 
 
 def cart(request):
-    return render(request, 'cart/cart.html')
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+
+    context = {'items': items}
+    return render(request, 'cart/cart.html', context)
 
 
 def checkout(request):
