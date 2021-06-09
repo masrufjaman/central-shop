dd6f6b5c8506605ac762ff80be6b7c1e0a6d1295
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-06-09 19:38:04 +0600

    carts total

--- cart/views.py
+++ cart/views.py
@@ -11,8 +11,9 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
 
-    context = {'items': items}
+    context = {'items': items, 'order': order}
     return render(request, 'cart/cart.html', context)
 
 
