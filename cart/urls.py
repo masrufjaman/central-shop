9ab6917474b15b6f6b5ce6f4904d918bec18c053
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-06-09 16:51:48 +0600

    checkout urls

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cart, name='cart-cart')
+    path('', views.cart, name='cart-cart'),
+    path('checkout/', views.checkout, name='cart-checkout'),
 ]
