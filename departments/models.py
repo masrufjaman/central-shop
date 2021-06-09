dbaf3d1a960c9e6429ff2f385a99ad2894b42301
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-06-09 14:52:36 +0600

    render product images

--- departments/models.py
+++ departments/models.py
@@ -9,3 +9,11 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
