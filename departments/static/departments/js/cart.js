b29e523b4546ddebb0cd494fc28b52f0db452d22
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-06-15 20:32:40 +0600

    added login or logout info

--- departments/static/departments/js/cart.js
+++ departments/static/departments/js/cart.js
@@ -5,6 +5,13 @@ for (var i = 0; i < updateBtns.length; i++) {
         var productId = this.dataset.product
         var action = this.dataset.action
         console.log('productId:', productId, 'action:', action)
+
+        console.log('USER:', user)
+        if(user === 'AnonymousUser'){
+            console.log('Not logged in')
+        }else{
+            console.log('User is logged in, sending data')
+        }
     })
     
 }
